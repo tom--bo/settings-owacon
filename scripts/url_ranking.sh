ffcfde5a7e6f10b811378e2e5a75d354aa27d5de
osjupiter <water_totuka@yahoo.co.jp>
2016-08-06 02:15:07 +0900

    read pipe or openfile in url_ranking

--- scripts/url_ranking.sh
+++ scripts/url_ranking.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 #This script prints urls sort by access times
 
+_rank(){
 awk -F'\t' '{print $6}' < /dev/stdin | sort | uniq -c
+}
+
+if [ -p /dev/stdin ]; then
+        cat -
+    else
+        cat $@
+fi | _rank
