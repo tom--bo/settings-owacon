6e532d1be0bef86fe7f2a7bf72ad09efc6ff8509
tom--bo <otsuka.t.2013@gamil.com>
2016-09-21 23:14:15 +0900

    Fix tar directory

--- scripts/install-go1.7.sh
+++ scripts/install-go1.7.sh
@@ -2,7 +2,7 @@
 
 wget https://storage.googleapis.com/golang/go1.7.linux-amd64.tar.gz -O /tmp/go1.7.linux-amd64.tar.gz
 
-tar xvf /tmp/go1.7.linux-amd64.tar.gz
+tar xvf /tmp/go1.7.linux-amd64.tar.gz -C /tmp
 sudo mv /usr/local/go /usr/local/old_go
 sudo mv /tmp/go /usr/local/go
 
