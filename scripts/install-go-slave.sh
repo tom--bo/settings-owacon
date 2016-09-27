2efef22007aadd4bb7c23651b5ca7556cd9b52a8
Kender09 <k.ikeuchi@dcl.cs.waseda.ac.jp>
2016-09-27 08:04:01 +0000

    install go sh

--- scripts/install-go-slave.sh
+++ scripts/install-go-slave.sh
@@ -3,8 +3,11 @@
 sudo wget https://storage.googleapis.com/golang/go1.7.linux-amd64.tar.gz -O /tmp/go1.7.linux-amd64.tar.gz
 
 sudo tar xvf /tmp/go1.7.linux-amd64.tar.gz
-if [  -d /usr/local/go  ]; then
-	sudo mv /usr/local/go /usr/local/old_go
-fi
 sudo mv go /usr/local
 
+echo 'GOROOT=/usr/local/go' >> ~/.zshrc
+echo 'GOPATH=$GOROOT/bin' >> ~/.zshrc
+echo 'PATH=$GOPATH:$PATH' >> ~/.zshrc
+echo export PATH GOPATH GOROOT >> ~/.zshrc
+
+source ~/.zshrc
