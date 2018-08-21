e746fc48a5196420f5cf5ca425fed817b5ef8c05
osjupiter <water_totuka@yahoo.co.jp>
2018-08-21 22:13:07 +0900

    install unzip

--- scripts/install-alp.sh
+++ scripts/install-alp.sh
@@ -4,7 +4,7 @@
 wget https://github.com/tkuchiki/alp/releases/download/v0.2.4/alp_linux_amd64.zip -O /tmp/alp_linux_amd64.zip
 
 # if you need
-#sudo apt-get install unzip
+sudo apt-get install unzip -y
 
 # copy alp into /usr/local/bin to install
 unzip /tmp/alp_linux_amd64.zip -d /tmp
